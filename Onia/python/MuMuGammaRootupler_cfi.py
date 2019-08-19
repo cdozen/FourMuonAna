0dbabaa438c417cd8a29f0c295e234592f3cf65b
Candan Dozen <candan.dozen@cern.ch>
2019-08-19 18:50:39 +0200

    added correct HLT strings for 2017 and 2016

--- Onia/python/MuMuGammaRootupler_cfi.py
+++ Onia/python/MuMuGammaRootupler_cfi.py
@@ -18,7 +18,7 @@ rootuple = cms.EDAnalyzer('MuMuGammaRootupler',
                           VERBOSE = cms.untracked.bool(True), 
                           triggerList = cms.untracked.vstring(
                                     'HLT_Trimuon5_3p5_2_Upsilon_Muon_v',       #2018  HLT string
-                                    'HLT_TrimuonOpen_5_3p5_2_Upsilon_Muon_v',  #2018 HLT string
+                                    #'HLT_TrimuonOpen_5_3p5_2_Upsilon_Muon_v',  #2018 HLT string
                                     #'HLT_Trimuon2_Upsilon5_Muon_v'            #2017 HLT string
                                     #'HLT_Dimuon0_Upsilon_Muon_v',             #2016 HLT string 
                                     #'HLT_Dimuon0_Phi_Barrel_v',               #2016 HLT string 
